d2a0042733822dc8a4e4febe95526d280a01fa5d
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-10 01:31:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-end
 
   get '/recipes/new' do
     erb :new
@@ -13,4 +12,3 @@ end
     erb :index
   end
 end
-
